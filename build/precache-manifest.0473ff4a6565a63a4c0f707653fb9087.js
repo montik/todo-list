2b818b92ec10a684b257e9e4cc1810d96fc98f49
Edo Monticelli <montik@autistici.org>
2019-05-28 01:08:06 +0200

    Add build dir

---
+++ build/precache-manifest.0473ff4a6565a63a4c0f707653fb9087.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8337d49515968e263279e98fa5d7b3b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70b065f7cb93c3b8ca4e",
+    "url": "/static/css/main.fd7b88c9.chunk.css"
+  },
+  {
+    "revision": "65b79d4090cfc9a36a9d",
+    "url": "/static/js/2.11911b22.chunk.js"
+  },
+  {
+    "revision": "70b065f7cb93c3b8ca4e",
+    "url": "/static/js/main.fc36c8ef.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
